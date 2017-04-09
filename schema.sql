c7c0b8a262d9e09a37459740d4a66d997da14577
Ichwan Palongengi <ich9681@gmail.com>
2017-04-09 11:58:50 -0400

    add the barebone schema for Segments and Stations tables

--- schema.sql
+++ schema.sql
@@ -1,1 +1,13 @@
-CREATE TABLE students
+CREATE TABLE Stations (
+  station_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  station_name TEXT NOT NULL,
+  station_code TEXT NOT NULL
+);
+
+CREATE TABLE Segments (
+  segment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  segment_north INTEGER NOT NULL,
+  segment_south INTEGER NOT NULL,
+  FOREIGN KEY (segment_north) REFERENCES Stations(station_id),
+  FOREIGN KEY (segment_south) REFERENCES Stations(station_id)
+);
