60715d0c5419bd7847e43acdfed82b787a7248f0
Adedamola Shomoye <dshomoye@gmail.com>
2017-04-28 22:43:05 -0400

    clean start for Dabi

---
+++ app/app/models.py
@@ -0,0 +1,213 @@
+import sqlite3 as sql
+import json
+import collections
+import os
+from datetime import datetime
+
+# This file is basically how the webserver interfaces with the database
+# we'll have to replace these with our own functions working on our own database
+# for now I'm leaving them here for reference
+
+
+'''
+create new passenger
+'''
+def create_passenger(p_lname,p_fname,p_address,p_email):
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO Passengers (passenger_lname,passenger_fname,passenger_billing_address,passenger_email) VALUES (?,?,?,?);", (p_lname, p_fname,p_address,p_email))
+        con.commit()
+        cur.execute("SELECT LAST_INSERT_ID();")
+        return cur.fetchall()[0][0]
+
+'''create ticket
+'''
+def create_ticket(start_station,end_station,train_num,trip_date_time,passenger_id, fare, round_trip, return_train, return_date_time):
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        q=("INSERT INTO Tickets(trip_starts, trip_ends, trip_train, trip_date, passenger_id," 
+            "round_trip, return_train, return_date, fare)"
+            " VALUES "
+            " (?,?,?,?,?,?,?,?,?);"
+            )
+        cur.execute(q,(start_station,end_station,train_num,trip_date_time,passenger_id,round_trip,return_train,return_date_time,fare))
+        con.commit()
+        cur.execute("SELECT LAST_INSERT_ROWID();")
+        return cur.fetchall()[0][0]
+
+'''
+ update free seats for all segments in the given trip
+'''
+def update_free_seats(start_station,end_station,train_num,date):
+    if(start_station>end_station): start_station,end_station=end_station,start_station
+    ## this will be done in mysql but we can't do stored procedures in sqlite so... :/
+    next_station=start_station+1
+    while(next_station<=end_station):
+        with sql.connect("database.db") as con:
+            q = ("update Seats_free "
+                "SET sf_seats_free = sf_seats_free-1 "
+                "WHERE sf_train_num=? AND sf_date=? AND " 
+                "sf_seg_id in (SELECT segment_id FROM Segments WHERE " 
+                "(segment_north=? AND segment_south=?) OR (segment_south=? AND segment_north=?)); "
+                )
+            cur = con.cursor()
+            cur.execute(q,(train_num, date, start_station, next_station, start_station, next_station))
+            con.commit()
+        start_station=next_station
+        next_station+=1
+
+
+
+'''
+    method for getting station id from station symbol
+'''
+def get_station_id(stationcode):
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        # need the comma here or python doesn't recognize (stationcode) as a tuple
+        cur.execute("SELECT station_id from Stations WHERE station_code =?", (stationcode,))
+        return cur.fetchall()[0][0]
+
+
+'''
+    method for getting all trains running from stations at a given date and optional time of day
+'''
+def get_trains_from_station(start_station,end_station,date,time_of_day=None):
+    #set the time interval for time of day
+    if(time_of_day=="morning"):
+        start_train_time = '00:00:00'
+        end_train_time = '11:59:59'
+    elif(time_of_day=="afternoon"):
+        start_train_time = '12:00:00'
+        end_train_time = "17:00:00"
+    elif (time_of_day=="evening"):
+        start_train_time = "17:00:00"
+        end_train_time = "23:59:59"
+    else:
+        start_train_time = "00:00:00"
+        end_train_time = "23:59:59"
+
+    if(start_station>end_station):
+        direction = 1
+    else: direction = 0
+    #weekday/weekend trains
+    day = pydate(date).weekday()
+    if(day<5): 
+        day = "MF"
+    else: day = "SSH"
+    
+    with sql.connect("database.db") as con:
+        cur  = con.cursor()
+        squery=("select sa.train_num, sa.time_out from stops_at sa "
+                "join trains t "
+                "on (t.train_num=sa.train_num AND t.direction=? AND t.train_days=?) "
+                "where "
+                "sa.station_id = ? AND sa.time_out between ? AND ?; "
+                )
+
+        cur.execute(squery,
+            (direction, day, start_station, start_train_time,end_train_time))
+
+        a_trains=list(cur.fetchall())
+        trains_list = []
+        for t in a_trains:
+            train = {}
+            train['train_num'] = t[0]
+            train['time_out'] = t[1]
+
+            trains_list.append(train)
+        return trains_list
+
+
+def check_free_seats(start_station, end_station, train_num, date):
+    #start_station; end_station; train_num; date #from user
+    if(end_station<start_station): start_station,end_station = end_station,start_station
+
+    next_station = start_station+1
+    while (next_station<=end_station):
+        #find segment id with north-end = start_station and south_end = next_station
+        #use seg_id, date and train_num to find free seats
+        ## the two actions above should be a SQL stored procedure executed in one line in python
+        ### stored procedure returns boolean for free or not free.
+        # nope.
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            # find the one segment with matching north and south stations
+            # query the number of seats it has free for x train on y date
+            # TODO: make this less clunky as sin
+            cur.execute("SELECT sf_seats_free FROM Seats_Free WHERE sf_train_num=? AND sf_date=? AND sf_seg_id in (SELECT segment_id FROM Segments WHERE (segment_north=? AND segment_south=?) OR (segment_south=? AND segment_north=?))", 
+                (train_num, date, start_station, next_station, start_station, next_station))
+        ## store in local var free_seats
+
+        ##TODO change this to retrieve value (fetchone()[0]) when there are values in seats_free
+            free_seats = cur.fetchone()
+            if(not free_seats): 
+                return False 
+        start_station=next_station
+        next_station+=1
+    return True
+
+# we might need these later. i'm shoving them here just in case
+# we can sort out the details as we go along
+
+
+def sqldate(date):
+    # takes python datetime object and converts to string for submission to sql queries
+    # TODO: remake schema.sql to use datetime where appropriate
+    # TODO: create format string e.g. formatstring = '%b %d %Y %I:%M%p'
+    # look up strptime in datetime documentation to see what i mean
+    
+    formatstring = "%Y-%m-%d"
+    dt = date.strftime(formatstring)
+    return dt
+
+def pydate(date):
+    formatstring = "%Y-%m-%d"
+    # takes string and converts to datetime object for use in python functions
+    return datetime.strptime(date, formatstring)
+
+
+'''
+Getting passengers reservations and handling all previous tickets
+'''
+
+
+def get_passenger_reservation(passengerID):
+    with sql.connect('database.db') as con:
+        cur = con.cursor()
+        cur.execute("""SELECT t.trip_id, t.trip_train, ls.station_name origin, le.station_name destination, 
+                        t.trip_date, t.round_trip, t.return_train, t.return_date, t.fare, t.cancelled
+                        FROM Tickets t JOIN Stations ls
+                        ON ls.station_id = t.trip_starts
+                        JOIN Stations le
+                        ON le.station_id = t.trip_ends  WHERE t.passenger_id = ?""",
+                    (passengerID,))
+        result = cur.fetchall()
+        return result
+
+
+def rebook_ticket(ticketID):
+    with sql.connect('database.db') as con:
+        cur = con.cursor()
+        query_stmt = ("INSERT INTO Tickets (trip_starts, trip_ends, trip_train, trip_date, passenger_id, round_trip, return_trip, return_train, return_date, fare)"
+                      "SELECT (trip_starts, trip_ends, trip_train, trip_date, passenger_id, round_trip, return_trip, return_train, return_date, fare)"
+                      "FROM Tickets"
+                      "WHERE trip_id = ?")
+        cur.execute(query_stmt, (ticketID, ))
+        con.commit()
+
+
+def cancel_ticket(ticketID):
+    with sql.connect('database.db') as con:
+        cur = con.cursor()
+        query_stmt = ("UPDATE Tickets SET cancelled = 1 WHERE trip_id = ?")
+        cur.execute(query_stmt, (ticketID, ))
+        con.commit()
+
+
+def get_ticket_record(ticketID):
+    with sql.connect('database.db') as con:
+        cur = con.cursor()
+        query_stmt = ("SELECT * FROM Tickets WHERE trip_id = ?")
+        cur.execute(query_stmt, (ticketID,))
+        return cur.fetchone()
---
+++ app/app/views.py
@@ -0,0 +1,172 @@
+from app import app
+import datetime as dt
+#import json
+from flask import render_template, redirect, request, flash, g, session, url_for, send_file
+from models import *
+
+# don't ask what this is for. i don't remember either.
+app.secret_key = "donttellanyonemysecret"
+
+# home page
+# see templates directory for what this whole templates thing is about
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
+
+# left the username/password code in just in case
+def index():
+    if 'username' in session:
+        return render_template("index.html",username =session['username'])
+    else:
+        return render_template("index.html")
+
+@app.route('/search_results', methods=["GET", "POST"])
+def search_results():
+    if(request.method=='POST'):
+        session['trip_time_of_day'] = request.form['triptime']
+        session['start_station_name'] = request.form["startstation"]
+        session['start_station']=get_station_id(session['start_station_name'])
+        session['end_station_name'] = request.form["endstation"]
+        session['end_station']=get_station_id(session['end_station_name'])
+        session['trip_date'] = request.form["tripdate"]
+        session['trip_type'] = request.form['roundtrip']
+        session['return_booking'] = False
+        #initialize these values to None to avoid index error
+        session['return_train'] = session['return_time'] = None
+        ##to retrieve across pages
+
+        if(session['trip_type']=="round"):
+            session['return_time_of_day'] = request.form['returntime']
+            session['return_date'] = request.form['returndate']
+            return_time_of_day = session['return_time_of_day'] 
+            return_date = session['return_date'] 
+
+    trip_time_of_day = session['trip_time_of_day'] 
+    start_station = session['start_station'] 
+    end_station = session['end_station'] 
+    trip_date = session['trip_date'] 
+    trip_type = session['trip_type']
+    session['fare'] = abs(start_station - end_station)*2 
+ 
+
+    #to handle getting search results for return trips - can also be a separate view but whatever
+    if(session['return_booking']):
+        start_station=session['return_start_station']
+        end_station=session['return_end_station']
+        trip_date = session['return_date']
+        trip_time_of_day = session['return_time_of_day']
+        session['fare']*=2
+
+
+    #dictionary (train number and train time) for all trains running from station     
+    all_trains = get_trains_from_station(start_station,end_station,trip_date,trip_time_of_day)
+    ## doesn't work yet, no data on free seats
+    free_trains=[]
+    for t in all_trains:
+        if(check_free_seats(start_station,end_station,t['train_num'],trip_date)):
+            free_trains.append(t)
+
+    ##Change to return only free trains
+    return render_template("search_results.html",all_trains=free_trains, trip_date=trip_date, trip_time_of_day=trip_time_of_day, start_station = start_station, end_station = end_station, )
+
+## get user (or new user) info and send to confirmation through the form.
+@app.route("/book/<int:train_num>/<train_time>")
+def booktrip(train_num,train_time):
+    if(session['return_booking']):
+        session['return_train'] = train_num
+        session['return_time'] = train_time
+    else:
+        session['trip_train']=train_num
+        session['trip_time'] = train_time
+    ## the user is coming from search results check if they still need to choose return trip
+    if(session['trip_type'] == "round" and not session['return_booking']):
+        #call choose return to get return booking
+        return choosereturn()
+    else:
+        return render_template("passenger_trip.html", start_station=session['start_station_name'],end_station=session['end_station_name']);
+
+
+
+## re render search results for the return trip
+@app.route("/returntrip/<return_time_of_day>/<return_date>")
+def choosereturn():
+    session['return_booking'] = True
+    session['return_start_station'] = session['end_station']
+    session['return_end_station'] = session['start_station']
+    return search_results()
+
+
+'''
+this will be the thank you page
+decrement free seats for segments
+create new passenger from form if needed
+create a ticket from the passenger id returned from the booking page.  - 
+some of the needed methods are already in models, might need testing though
+render page that shows trip info and passenger id
+'''
+@app.route('/confirmation', methods=["POST"])
+def confirm_book():
+    result = request.form
+    #check if new user or returning
+    if('passenger_id' not in result):
+        p_id = create_passenger(result['lname'],result['fname'],result['email'],result['address'])
+    else:
+        p_id = result['passenger_id']
+    #update free seats for both trips
+    update_free_seats(session['start_station'],session['end_station'],session['trip_train'],session['trip_date'])
+
+    trip_date_time = ("%s %s" % (session['trip_date'], session['trip_time']))
+
+    #update values for return trip
+    if(session['return_booking']):
+        update_free_seats(session['end_station'],session['start_station'],session['return_train'],session['return_date'])
+        return_date_time = ("%s %s" % (session['return_date'], session['return_time']))
+    else: return_date_time = None
+
+    #create ticket for user
+    ticket_num = create_ticket(session['start_station'],session['end_station'],
+        session['trip_train'],trip_date_time,p_id,session['fare'],session['return_booking'],session['return_train'],return_date_time,)
+    return render_template("confirmation.html",passenger_id=p_id,ticket_num =ticket_num)
+
+
+# keep these since we might want to use css/images/js in our stuff
+@app.route("/css/<css>")
+def style_render(css):
+    return render_template("css/%s"%css)
+
+@app.route("/images/<image>")
+def image_render(image):
+    filename = ('templates/images/%s'%image)
+    return send_file(filename,mimetype='image/jpeg')
+
+@app.route("/js/<js>")
+def js_render(js):
+    return render_template("js/%s"%js)
+
+# don't know what this is so it's probably important
+@app.route("/js/moment-develop/moment.js")
+def moment_js_render():
+    return render_template("/js/moment-develop/moment.js")
+
+# bootstrap stuff
+@app.route("/bootstrap/js/<js>")
+def bootstrap_js_render(js):
+    return render_template("/bootstrap/js/%s"%js)
+
+@app.route("/bootstrap/dist/bootstrap.min.js")
+def bootstrapminjs():
+    return render_template("bootstrap/dist/js/bootstrap.min.js")
+
+@app.route("/bootstrap-datetimepicker.min.js")
+def timepickerjs():
+    return render_template("bootstrap-datetimepicker.min.js")
+
+
+
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if 'username' in session:
+        return render_template("login.html",username=session['username'])
+    else:
+        return render_template("login.html")
+
