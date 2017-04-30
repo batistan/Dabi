201974096e56a8b0aaea409df46422c37b14db91
batistan <nbatist000@citymail.cuny.edu>
2017-04-30 18:57:44 -0400

    Added train_delay function

--- app/models.py
+++ app/models.py
@@ -40,3 +40,18 @@ def get_ticket_record(ticketID):
         query_stmt = ("SELECT * FROM Tickets WHERE trip_id = ?")
         cur.execute(query_stmt, (ticketID,))
         return cur.fetchone()
+
+def delay_train(trainID, amt, station):
+    with sql.connect('database.db') as con:
+        cur = con.cursor()
+        query_stmt = ("CREATE TABLE Temp_Stops_At AS"
+                      "SELECT * FROM Stops_At")
+        cur.execute(query_stmt)
+
+        query_stmt = ("SELECT station_id FROM Temp_Stops_At where train_num = ? AND station_id >= ? ORDER BY station_id ASC")
+        cur.execute(query_stmt, (trainID, station))
+
+        for st in cur.fetchall():
+            query_stmt = ("UPDATE Temp_Stops_At SET time_in = TIME(time_in, '+? minute'), time_out = TIME(time_out,'+? minute') WHERE train_num = ? AND station_id = ?")
+            cur.execute(query_stmt, (amt, amt, trainID, st))
+
